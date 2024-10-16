ea1bcce6e705c13e29de84a1cc2365b40b8b6ae2
Yuriy <yuriy.bodolanov@gmail.com>
2024-10-16 18:41:47 +0400

    created all the error handling

--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,7 @@ fn main() {
         stdin().read_line(&mut input).expect("Something wrong with input itself");
         let input = input.trim();
 
-        if input.is_empty() {
+        if input.trim().is_empty() {
             println!("Type something");
             continue;
         }
@@ -65,43 +65,59 @@ fn handle_input_command(input: &str, budgets: &mut Budgets, current_budget: &mut
 
 fn create_new_budget(budgets: &mut Budgets) {
     let mut name = String::new();
-    let mut amount = String::new();
+    let mut amount_input = String::new();
+    let amount: i32;
 
-    println!("Enter the name of the budget, please");
-    stdin().read_line(&mut name).expect("Error with entering the name");
-    let name = name.trim();
-
-    if name.is_empty() {
-        println!("You need to specify the budget name");
-        return; // Change on loop
+    loop {
+        name.clear();
+        println!("Enter the name of the budget, please");
+        stdin().read_line(&mut name).expect("Error with entering the name");
+    
+        if name.trim().is_empty() {
+            println!("You need to specify the budget name");
+            continue;
+        } else {
+            break;
+        }
     }
 
-    println!("Enter the amount of available money for budget");
-    stdin().read_line(&mut amount).expect("Error with entering the amount");
-    
-    let amount = match amount.trim().parse::<i32>() {
-        Ok(value) => value,
-        Err(_) => {
-            println!("Error parsing the amount");
-            return; // Change on loop
-        }
-    };
+    loop {
+        amount_input.clear();
+        println!("Enter the amount of available money for budget");
+        stdin().read_line(&mut amount_input).expect("Error with entering the amount");
+        
+        match amount_input.trim().parse::<i32>() {
+            Ok(value) => {
+                amount = value;
+                break;
+            },
+            Err(_) => {
+                println!("Enter the amount by numbers, please...");
+                continue;
+            }
+        };
+    }
 
-    let budget = Budget::new(name.to_string(), amount);
+    let budget = Budget::new(name.trim().to_string(), amount);
     budgets.add_budget(budget);
-    println!("The budget is created: {}", name);
+    println!("The budget is created: {}", name.trim());
 }
 
 
 fn show_budgets(budgets: &Budgets) -> () {
-    for budget in budgets.budgets.iter() {
-        println!("{}", budget.name);
+    check_budgets(budgets);
+
+    for (i, budget) in budgets.budgets.iter().enumerate() {
+        println!("{}. - {}", i + 1, budget.name);
     }
 }
 
 
 fn get_budget(budgets: &Budgets, current_budget: &mut Option<usize>) -> () {
+    check_budgets(budgets);
+
     println!("Choose the budget by entering the number");
+    show_budgets(budgets);
     
     loop {
 
@@ -132,15 +148,33 @@ fn get_budget(budgets: &Budgets, current_budget: &mut Option<usize>) -> () {
 
 
 fn show_ballance(budgets: &Budgets, current_budget: &mut Option<usize>) -> () {
+    check_current_budget(current_budget);
+    
     let ballance = budgets.budgets.get(current_budget.unwrap()).unwrap().get_budget();
     println!("The current ballance is: {}", ballance);
 }
 
 
 fn add_to_budget(budgets: &mut Budgets, current_budget: &mut Option<usize>) -> () {
-    let mut amount = String::new();
-    stdin().read_line(&mut amount).expect("Error reading the amoung");
-    let amount = amount.trim().parse::<i32>().expect("Error, parsing the value");
+    check_current_budget(current_budget);
+    check_budgets(budgets);
+
+    let amount: i32;
+    loop {
+        let mut amount_input = String::new();
+        stdin().read_line(&mut amount_input).expect("Error reading the amoung");
+
+        match amount_input.trim().parse::<i32>() {
+            Ok(value) => {
+                amount = value;
+                break;
+            },
+            Err(_) => {
+                println!("Enter the number, please");
+                continue;
+            }
+        };
+    }
 
     budgets.budgets
         .get_mut(current_budget.unwrap())
@@ -150,9 +184,25 @@ fn add_to_budget(budgets: &mut Budgets, current_budget: &mut Option<usize>) -> (
 
 
 fn remove_from_budget(budgets: &mut Budgets, current_budget: &mut Option<usize>) -> () {
-    let mut amount = String::new();
-    stdin().read_line(&mut amount).expect("Error reading the amount");
-    let amount = amount.trim().parse::<i32>().expect("Error, parsing the value");
+    check_current_budget(current_budget);
+    check_budgets(budgets);
+
+    let amount: i32;
+
+    loop {
+        let mut amount_input = String::new();
+        stdin().read_line(&mut amount_input).expect("Error reading the amount");
+        match amount_input.trim().parse::<i32>() {
+            Ok(value) => {
+                amount = value;
+                break;
+            },
+            Err(_) => {
+                println!("Error, parsing the value");
+                continue;
+            }
+        }
+    }
 
     budgets.budgets
         .get_mut(current_budget.unwrap())
@@ -162,6 +212,9 @@ fn remove_from_budget(budgets: &mut Budgets, current_budget: &mut Option<usize>)
 
 
 fn show_transactions(budgets: &mut Budgets, current_budget: &mut Option<usize>) -> () {
+    check_current_budget(current_budget);
+    check_budgets(budgets);
+
     budgets.budgets
         .get(current_budget.unwrap())
         .unwrap()
@@ -170,6 +223,9 @@ fn show_transactions(budgets: &mut Budgets, current_budget: &mut Option<usize>) 
 
 
 fn edit_transaction(budgets: &mut Budgets, current_budget: &mut Option<usize>) -> () {
+    check_current_budget(current_budget);
+    check_budgets(budgets);
+
     println!("Enter the number of transaction");
         let mut index = String::new();
         stdin().read_line(&mut index).expect("Error reading the index");
@@ -200,7 +256,26 @@ fn edit_transaction(budgets: &mut Budgets, current_budget: &mut Option<usize>) -
 
 
 fn delete_budget(budgets: &mut Budgets, current_budget: &mut Option<usize>) {
+    check_current_budget(current_budget);
+    check_budgets(budgets);
+    
     budgets.budgets.remove(current_budget.unwrap());
+}
+
+
+fn check_current_budget(current_budget: &mut Option<usize>) -> () {
+    if *current_budget == None {
+        println!("Choose the budget to operate first, please\n type \"get budget\"");
+        return;
+    }
+}
+
+
+fn check_budgets(budgets: &Budgets) -> () {
+    if budgets.budgets.len() == 0 {
+        println!("You haven't created any budget yet. Please type \"new budget\"");
+        return;
+    }
 }
 
 
